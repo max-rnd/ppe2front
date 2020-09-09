d3c4ac1c9f2da49049cf56f1f9479714bc9a0d55
Max RENAUD <contact@max-renaud.fr>
2020-09-09 10:26:56 +0200

    finnal_commit

--- js/admin.js
+++ js/admin.js
@@ -4,11 +4,11 @@ $(function () {
         url: 'http://localhost:8080/allexpo',
         dataType: 'json',
         success: function (expos) {
-            // Création des méthode / fonction
             selectExpo = document.getElementById('selectExpo');
             for (i = 0; i < expos.length; i++) {
-                selectExpo.options[selectExpo.options.length] = new Option(expos[i]['titre'], i);
+                selectExpo.options[selectExpo.options.length] = new Option(expos[i]['titre'], i );
             } // Chargement de la liste des expositions
+
             function selectedExpo() {
                 return expos[$("#selectExpo").children("option:selected").val()];
             } // Fonction qui retourne l'exposition sélectionné
@@ -36,17 +36,17 @@ $(function () {
                 expo['titre'] = $("#titreExpo").val();
                 expo['dateDebut'] = $("#dateDebut").val();
                 expo['dateFin'] = $("#dateFin").val();
-                expo['noteComm'] = CKEDITOR.instance.textNoteComm.getData();
+                expo['noteComm'] = CKEDITOR.instances.textNoteComm.getData();
+                expo['artiste'] = selectedExpo()['artiste']; // Pour l'instant on ne peut pas changer l'artiste associé
                 return expo;
             }  // Fonction de récupération des éléments saisie (ou modifié) par l'utilisateur
 
-            // Début
             CKEDITOR.replace('textNoteComm'); // Inicialisation CKEditor (remplace le textarea #textNoteComm)
             seeElemExpo(setCacheExpo());
-            $("#noteComm").hide(); // De base la note du commissaire est masqué
             $("#selectExpo").change(function () {
                 seeElemExpo(setCacheExpo());
             }); // Quand l'utilisateur change l'exposition sélectionné
+            $("#noteComm").hide(); // De base la note du commissaire est masqué
             $("#showNoteComm").click(function () {
                 if ($("#showNoteComm").attr("value") == 0) {
                     $("#showNoteComm").attr("value", 1);
@@ -60,6 +60,7 @@ $(function () {
             }); // Quand on click sur "Afficher / Masquer la note du commissaire"
             $("#expoUpdate").click(function () {
                 expo = getEditExpo();
+                console.log(expo);
                 if ( // Véfication pour voir si des champs son vide (a par la note du commissair)
                     expo['titre'].trim() == "" ||
                     expo['dateDebut'] == "" ||
@@ -72,17 +73,18 @@ $(function () {
                     if (expo['dateFin'] == "")
                         $("#dateFin").addClass("is-invalid");
                 } else {
+                    oldExpo = selectedExpo();
                     if ( // Vérification pour voir si les valeurs ont changées
-                        newVal['titre'] == val[idExpo - 1]['titre'] &&
-                        newVal['dateDebut'] == val[idExpo - 1]['dateDebut'] &&
-                        newVal['dateFin'] == val[idExpo - 1]['dateFin'] &&
-                        newVal['noteComm'] == val[idExpo - 1]['noteComm']
+                        expo['titre'] == oldExpo['titre'] &&
+                        expo['dateDebut'] == oldExpo['dateDebut'] &&
+                        expo['dateFin'] == oldExpo['dateFin'] &&
+                        expo['noteComm'] == oldExpo['noteComm']
                     ) {
                         generAlert('warning', "L'exposition n'a pas été mis à jour, rien n'a été changé");
                     } else {
                         $.ajax({
                             type: 'POST',
-                            url: 'http://localhost:8080/expo/' + idExpo + '/edit',
+                            url: 'http://localhost:8080/expo/' + selectedExpo()['id'] + '/edit',
                             data: expo,
                             success: function (expoRe) {
                                 if (expoRe['id'] == null)
@@ -93,30 +95,19 @@ $(function () {
                         });
                     }
                 }
-            }); // Quand on click sur "Mettre à jour" (l'expo)
-
+            }); // Quand on click sur "Mettre à jour"
             updateVerif($("#titreExpo"));
             updateVerif($("#dateDebut"));
             updateVerif($("#dateFin"));
-
-            // Début partie ARTISTE
-            $.ajax({
-                type: 'GET',
-                url: 'http://localhost:8080/allartiste',
-                dataType: 'json',
-                success: function (val) {
-                    selectArtiste = document.getElementById('selectArtiste');
-                    for (i = 0; i < val.length; i++) {
-                        selectArtiste.options[selectArtiste.options.length] = new Option(val[i]['nom'], val[i]['id']);
-                    }
-                }
+            $("#newExpo").click(function () {
+                alert("ya");
             });
         },
         error: function () {
             alert("Erreur de chargement");
         }
     });
-    // Fonctions globale
+
     function generAlert(type, msg) {
         $("body").prepend(
             "<div class=\"alert alert-" + type + " m-4 col-md-4 mx-auto fixed\" id=\"alert\">" + msg + "</div>"
@@ -132,15 +123,4 @@ $(function () {
 
     } // Enlève la class "is-invalid" quand l'utilisateur remplie le champ
 
-    /*function setElemExpo() {
-        dd = new Date(val['dateDebut']);
-        if (dd >= Date.now())
-            $("#expoStatus").html("Status : A venir");
-        else
-            $("#expoStatus").html("Status : En cours");
-
-        $("#titreExpo").attr("value", val['titre']);
-        $("#dateDebut").attr("value", val['dateDebut']);
-        $("#dateFin").attr("value", val['dateFin']);
-    }*/
 });
--- js/ajax.js
+++ js/ajax.js
@@ -2,7 +2,9 @@ $(function () {
     $.ajax({
         type: 'GET',
         url: 'http://localhost:8080/expo',
+        dataType: 'json',
         success: function (val) {
+            var idArtiste = val['artiste'];
             var mois = new Array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre");
             var dateDebut = new Date(val['dateDebut']);
             var dateFin = new Date(val['dateFin']);
@@ -11,26 +13,54 @@ $(function () {
             $("#titreExpo").html(val['titre']);
             $("#dateExpo").html("Du " + dd + " au " + df);
             $("#noteComm").html(val['noteComm']);
+            $.ajax({
+                type: 'GET',
+                url: 'http://localhost:8080/artiste/' + idArtiste,
+                dataType: 'json',
+                success: function (val) {
+                    $("#nomArtiste").html(val['nom']);
+                    $("#portrait").attr("src","image/portraits/" + val['portrait']);
+                    $("#bioResu").html(val['resuBio']);
+                    $("#bioDetaille").html(val['bio']);
+                }
+            });
+            $.ajax({
+                type: 'GET',
+                url: 'http://localhost:8080/oeuvres/' + idArtiste,
+                dataType: 'json',
+                success: function (val) {
+                    $.each(val, function (i) {
+                        var d = new Date(val[i]['date']);
+                        $("#galerie").append(
+                            "<div class=\"box color1\"><div><img src=\"image/oeuvres/" +
+                            val[i]['image'] +
+                            "\"></div><p id=\"txt1\">" +
+                            val[i]['nom'] + "<br/>" + d.getFullYear() +
+                            "</p></div>"
+                        );
+                    });
+                }
+            });
+            $.ajax({
+                type: 'GET',
+                url: 'http://localhost:8080/films/' + idArtiste,
+                dataType: 'json',
+                success: function (val) {
+                    $.each(val, function (i) {
+                        var d = new Date(val[i]['date']);
+                        $("#films").append(
+                            "<div class=\"box color2\"><div><img src=\"image/films/" +
+                            val[i]['image'] +
+                            "\" id=\"imgFilm1\"></div><div>" +
+                            val[i]['description'] +
+                            "</div></div>"
+                        );
+                    });
+                }
+            });
         },
         error: function () {
             alert("Ajax error : exposition");
         }
     });
-    $.ajax({
-        type: 'GET',
-        url: 'http://localhost:8080/expo',
-        success: function (val) {
-            var mois = new Array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre");
-            var dateDebut = new Date(val['dateDebut']);
-            var dateFin = new Date(val['dateFin']);
-            var dd = dateDebut.getDate() + " " + mois[dateDebut.getMonth()];
-            var df = dateFin.getDate() + " " + mois[dateFin.getMonth()];
-            $("#titreExpo").html(val['titre']);
-            $("#dateExpo").html("Du " + dd + " au " + df);
-            $("#noteComm").html(val['noteComm']);
-        },
-        error: function () {
-            alert("Ajax error : exposition");
-        }
-    });
-})
+});
